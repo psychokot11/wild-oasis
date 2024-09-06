6a3f964468d8dc8b3d59b381c7d8c0f83c87e933
Zofia Stawarska <z.stawarska@gmail.com>
2024-09-06 08:05:49 +0200

    connect supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,14 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+    const { data: cabins, error } = await supabase
+        .from('cabins')
+        .select('*')
+
+    if (error) {
+        console.error(error)
+        throw new Error('An error occurred while fetching cabins')
+    }
+
+    return cabins;
+}
---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseURL = 'https://rdirjjuufwlzuztyzhod.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkaXJqanV1ZndsenV6dHl6aG9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU1NTMxOTMsImV4cCI6MjA0MTEyOTE5M30.BTst9y9XV8l4D-1NDYd54ZC4y8fkZ_yR39qdR3QFSDU'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(supabaseURL, supabaseKey)
+
+export default supabase
