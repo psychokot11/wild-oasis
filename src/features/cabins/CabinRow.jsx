d4becb63a893fa4e1162b443c5db80ff80786c6d
Zofia Stawarska <z.stawarska@gmail.com>
2024-10-15 23:08:05 +0200

    compound components and render pattern

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -10,18 +10,7 @@ import {
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+import Table from "../../ui/Table";
 
 const Img = styled.img`
   display: block;
@@ -67,7 +56,7 @@ function CabinRow({ cabin }) {
   }
 
   return (
-      <TableRow role="row">
+      <Table.Row >
         <Img src={image} />
         <Cabin>{name}</Cabin>
         <div>Fits up to {maxCapacity}</div>
@@ -92,7 +81,7 @@ function CabinRow({ cabin }) {
           </Modal>
           
         </div>
-      </TableRow>
+      </Table.Row>
   )
 }
 
