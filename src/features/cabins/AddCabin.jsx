4e12bf93a79e33468ed2b39e6d9b5a739ec67aae
Zofia Stawarska <z.stawarska@gmail.com>
2024-10-15 20:11:40 +0200

    compound component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -2,20 +2,21 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 
+// export default AddCabin
 function AddCabin() {
-    const [isOpenModal, setIsOpenModal] = useState(false);
-
     return (
-        <>
-            <Button onClick={() => setIsOpenModal(true)}>Add new cabin</Button> 
-           {isOpenModal && (
-                <Modal onClose={() => setIsOpenModal(false)}>
-                    <CreateCabinForm onCloseModal={() => setIsOpenModal(false)}/>
-                </Modal>
-           )}
-            
-        </>
+        <div>
+            <Modal>
+                <Modal.Open opens="cabin-form">
+                    <Button>Add new cabin</Button>
+                </Modal.Open>
+                <Modal.Window name="cabin-form">
+                    <CreateCabinForm />
+                </Modal.Window>
+            </Modal>
+      </div>
     )
 }
 
